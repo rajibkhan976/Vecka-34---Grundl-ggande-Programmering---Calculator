71fbe9840e340376fca849be64c5f37fe4b05d62
unknown <rajibkhan976@gmail.com>
2019-08-25 18:17:28 +0200

    exception handling fixed

--- src/main/java/se/ecjava/rajib/Calculator.java
+++ src/main/java/se/ecjava/rajib/Calculator.java
@@ -1,5 +1,7 @@
 package se.ecjava.rajib;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,32 +12,36 @@ public class Calculator {
 
         boolean keepAlive = true;
         while (keepAlive) {
-            System.out.println("Enter the first number: ");
-            double inputOne = Double.parseDouble(scanner.nextLine());
-            System.out.println("Enter a mathematical operator");
-            String mathOperator = scanner.nextLine().trim();
-            System.out.println("Enter the second number: ");
-            double inputTwo = Double.parseDouble(scanner.nextLine());
-            switch (mathOperator) {
-                case "+":
-                    System.out.println(addOperation(inputOne, inputTwo));
-                    break;
-                case "-":
-                    System.out.println(subtractOperation(inputOne, inputTwo));
-                    break;
-                case "*":
-                    System.out.println(multiplyOperation(inputOne, inputTwo));
-                    break;
-                case "/":
-                    System.out.println(divideOperation(inputOne, inputTwo));
-                    break;
-                default:
-                    System.out.println("Unrecognized math operator!");
-            }
-            System.out.println("If you want to continue press 1 otherwise press 0");
-            int calculatorRunner = Integer.parseInt(scanner.nextLine());
-            if (calculatorRunner == 0) {
-                keepAlive = false;
+            try {
+                System.out.println("Enter the first number: ");
+                double inputOne = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter a mathematical operator");
+                String mathOperator = scanner.nextLine().trim();
+                System.out.println("Enter the second number: ");
+                double inputTwo = Double.parseDouble(scanner.nextLine());
+                switch (mathOperator) {
+                    case "+":
+                        System.out.println(addOperation(inputOne, inputTwo));
+                        break;
+                    case "-":
+                        System.out.println(subtractOperation(inputOne, inputTwo));
+                        break;
+                    case "*":
+                        System.out.println(multiplyOperation(inputOne, inputTwo));
+                        break;
+                    case "/":
+                        System.out.println(divideOperation(inputOne, inputTwo));
+                        break;
+                    default:
+                        System.out.println("Unrecognized math operator!");
+                }
+                System.out.println("If you want to continue press 1 otherwise press 0.");
+                int calculatorRunner = Integer.parseInt(scanner.nextLine());
+                if (calculatorRunner == 0) {
+                    keepAlive = false;
+                }
+            } catch (ArithmeticException | InputMismatchException | NumberFormatException e) {
+                System.out.println("Your input is invalid.");
             }
         }
     }
